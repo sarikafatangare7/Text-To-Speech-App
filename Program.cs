0ed0ff43254ff9d145cea2767451173cf6398c2f
SARIKA FATANGARE <87846955+sarikafatangare7@users.noreply.github.com>
2022-04-29 15:50:55 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace MySpeech_to_text_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my text to speech Application...");
+            SpeechSynthesizer spk = new SpeechSynthesizer();
+            spk.SetOutputToDefaultAudioDevice();
+            spk.Speak("Welcome to azue session, i hope you are learning somthing");
+        }
+    }
+}
